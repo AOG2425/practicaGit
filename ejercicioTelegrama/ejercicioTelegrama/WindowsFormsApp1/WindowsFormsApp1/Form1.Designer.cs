eff92dcf74fef08c2f6f1dad08c7567f12ed10e2
ALEJANDRA ORTIZ GAETE <aleortgae@alu.edu.gva.es>
2024-11-23 14:34:46 +0100

    punto 11

--- ejercicioTelegrama/ejercicioTelegrama/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ ejercicioTelegrama/ejercicioTelegrama/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp1
 {
-    partial class Form1
+    partial class Telegrama
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,13 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Label = new System.Windows.Forms.Label();
+            this.txtTelegrama = new System.Windows.Forms.TextBox();
+            this.chkUrgente = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Label
+            // 
+            this.Label.AutoSize = true;
+            this.Label.Location = new System.Drawing.Point(119, 77);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(43, 13);
+            this.Label.TabIndex = 0;
+            this.Label.Text = "TEXTO";
+            this.Label.Click += new System.EventHandler(this.TEXTO_Click);
+            // 
+            // txtTelegrama
+            // 
+            this.txtTelegrama.Location = new System.Drawing.Point(122, 110);
+            this.txtTelegrama.Multiline = true;
+            this.txtTelegrama.Name = "txtTelegrama";
+            this.txtTelegrama.Size = new System.Drawing.Size(512, 172);
+            this.txtTelegrama.TabIndex = 1;
+            // 
+            // chkUrgente
+            // 
+            this.chkUrgente.AutoSize = true;
+            this.chkUrgente.Location = new System.Drawing.Point(122, 288);
+            this.chkUrgente.Name = "chkUrgente";
+            this.chkUrgente.Size = new System.Drawing.Size(70, 17);
+            this.chkUrgente.TabIndex = 2;
+            this.chkUrgente.Text = "Urgente?";
+            this.chkUrgente.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 337);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Coste:";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(168, 337);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(175, 20);
+            this.txtPrecio.TabIndex = 4;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(463, 295);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(171, 62);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Telegrama
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chkUrgente);
+            this.Controls.Add(this.txtTelegrama);
+            this.Controls.Add(this.Label);
+            this.Name = "Telegrama";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Label;
+        private System.Windows.Forms.TextBox txtTelegrama;
+        private System.Windows.Forms.CheckBox chkUrgente;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Button btnCalcular;
     }
 }
 
--- ejercicioTelegrama/ejercicioTelegrama/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ ejercicioTelegrama/ejercicioTelegrama/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -10,11 +10,66 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class Telegrama : Form
     {
-        public Form1()
+        public Telegrama()
         {
             InitializeComponent();
+        }
+
+        private void TEXTO_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            string textoTelegrama;//uso de string ?¿? par leer el telegrama linea 28
+            char tipoTelegrama = 'o';// debo asignar como ordinario no vacio
+            int numPalabras = 0;
+            double coste;
+
+            textoTelegrama = txtTelegrama.Text;
+
+            if (chkUrgente.Checked)
+            {
+                tipoTelegrama = 'u';
+            }
+
+            string[] palabras = textoTelegrama.Split(' ');//lineas que sustitui por numPalabras = textoTelegrama.Length
+            numPalabras = palabras.Length;
+
+            if (tipoTelegrama == 'o')
+            {
+                if (numPalabras <= 10)
+                {
+                    coste = 2.5;
+                }
+                else
+                {
+                    coste = 2.5 /** 0.5*/ + (numPalabras - 10);//ANTES 0.5 * numPalabras; solo tenia el valor de c/palabra adicional
+                                                           //la sintaxis es parecida a la linea 59. AOG2425
+                }
+            }
+            else
+
+                if (tipoTelegrama == 'u')//AOG2425
+            {
+                if (numPalabras <= 5)//SOLO CONTABA 10 LETRAS-EL URGENTE ES MAYOR A 10 LETRAS Y AQUI NO IBA UN ELSE 
+
+                { coste = 5; }
+
+                else
+                {
+                    coste = 5 /*+ 0.75*/ * (numPalabras - 10);
+                }
+            }
+            else
+            {
+                coste = 0;
+            }
+
+            txtPrecio.Text = coste.ToString() + "euros";
         }
     }
 }
